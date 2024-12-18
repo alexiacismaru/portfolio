b4d3aca1656dd8254a724e45b04197b512f673a6
Alexia Cismaru <Alexia.cismaru@flowfactor.be>
2024-12-19 01:12:38 +0200

    Styling

--- portfolio/src/components/About.tsx
+++ portfolio/src/components/About.tsx
@@ -1,42 +1,5 @@
 // import { Dialog } from '@mui/material'; 
-
-const main = {
-    border: '4px solid blue', 
-    borderRadius: '7px'
-}
-
-const navigationMenu = {
-    backgroundColor: 'blue', 
-    display: 'flex', 
-    justifyContent: 'space-between'
-}
-
-const navButton = {
-    border: '1px solid white', 
-    backgroundColor: 'blue', 
-    width: '15px',
-    color: 'white', 
-    margin: '0.1rem'
-}
-
-const closeButton = {
-    border: '1px solid white', 
-    backgroundColor: 'red', 
-    width: '15px',
-    color: 'white', 
-    margin: '0.1rem'
-}
-
-const mocknav = {
-    display: 'flex', 
-    justifyContent: 'space-between', 
-    marginLeft: '1rem', 
-    backgroundColor: '#e3dac9',
-}
-
-const img = {
-    height: '1rem',
-}
+import '../css/About.css';  
 
 interface AboutProps {
     isOpen: boolean; 
@@ -46,76 +9,75 @@ interface AboutProps {
 export default function About({isOpen, onClose}: AboutProps) {
     return (
         <> 
-            {/* <Dialog style={main} open={isOpen} onClose={onClose}> */}
-            <main style={main}> 
-                <div className="navigation-menu" style={navigationMenu}>
-                    <div style={{display:'flex', alignItems: 'center'}}>
-                        <img src="https://i.imgur.com/RjMRqmK.png" alt='folder icon' style={{width: '25px', height: '25px'}}></img>
-                        <p style={{color: 'white'}}>C:\About</p>
+            {/* <Dialog id='main' open={isOpen} onClose={onClose}> */}
+            <main id='main-about'> 
+                <div id='nav'>
+                    <div id='about'>
+                        <img src="https://i.imgur.com/RjMRqmK.png" alt='folder icon'></img>
+                        <p>C:\About</p>
                     </div>
                     <div>
-                        <button style={navButton}>_</button>
-                        <button style={navButton}>O</button>
-                        <button style={closeButton} onClick={onClose}>X</button>
+                        <button>_</button>
+                        <button>O</button>
+                        <button style={{backgroundColor: 'red'}} onClick={onClose}>X</button>
                     </div> 
                 </div>
 
-                <div style={{backgroundColor: '#e3dac9'}}> 
-                    <div style={mocknav}>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '20%', fontSize: '1.5rem'}}>
-                            <p>File</p>
-                            <p>Edit</p>
-                            <p>View</p>
-                            <p>Insert</p>
-                            <p>Format</p>
-                            <p>Tools</p>
-                            <p>Table</p>
-                            <p>Help</p>
-                        </div> 
-                        <button>X</button> 
-                    </div>
+                <div id='mocknav'>
+                    <div style={{fontSize: '1.5rem'}}>
+                        <p>File</p>
+                        <p>Edit</p>
+                        <p>View</p>
+                        <p>Insert</p>
+                        <p>Format</p>
+                        <p>Tools</p>
+                        <p>Table</p>
+                        <p>Help</p>
+                    </div> 
+                    <button>X</button> 
                 </div>
-                <div style={{backgroundColor: '#e3dac9'}}> 
-                    <div style={mocknav}>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '20%'}}>
-                            <img style={img} src="http://i.imgur.com/6RErgJC.png"/>
-                            <img style={img} src="./src/assets/sticker-png-windows-xp-folders-pack-256-folder-opened-256-icon-thumbnail-removebg-preview.png"/>
-                            <img style={img} src="./src/assets/floppy-disk-removebg-preview.png"/> 
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/> 
+
+                <div> 
+                    <div id='mocknav'>
+                        <div>
+                            <img id='img' src="http://i.imgur.com/6RErgJC.png"/>
+                            <img id='img' src="./src/assets/sticker-png-windows-xp-folders-pack-256-folder-opened-256-icon-thumbnail-removebg-preview.png"/>
+                            <img id='img' src="./src/assets/floppy-disk-removebg-preview.png"/> 
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/> 
                         </div> 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
+                        <div>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
+                        <div>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/> 
+                        <div>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/> 
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/> 
+                        <div>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/> 
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
+                        <div>
                             <select name="zoom" id="zoom" style={{width: '100%'}}>
                                 <option value="100%">100%</option>
                                 <option value="75%">75%</option>
                                 <option value="50%">50%</option>
                                 <option value="25%">25%</option>
                             </select>
-                            <img style={img} src="./src/assets/email-icon.drawio.png"/>
+                            <img id='img' src="./src/assets/email-icon.drawio.png"/>
                         </div>
                         <button>X</button> 
                     </div>
                 </div> 
                 <div style={{backgroundColor: '#e3dac9'}}> 
-                    <div style={mocknav}>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '20%'}}>
+                    <div id='mocknav'>
+                        <div style={{}}>
                             <select name="font" id="font">
                                 <option value="Times New Roman">Times New Roman</option>
                                 <option value="Arial">Arial</option>
@@ -131,25 +93,25 @@ export default function About({isOpen, onClose}: AboutProps) {
                                 <option value="20">20</option>
                             </select>
                         </div> 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
+                        <div>
                             <p>B</p>    
                             <p>I</p>
                             <p>U</p>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button> 
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button> 
                         </div>
                         <button>X</button> 
                     </div>
@@ -160,8 +122,8 @@ export default function About({isOpen, onClose}: AboutProps) {
                     <p>Hi, I'm Alexia. I'm a software engineer and I like to make things.</p>
                 </section>
                 <div style={{backgroundColor: '#e3dac9'}}> 
-                    <div style={mocknav}>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '20%'}}>
+                    <div id='mocknav'>
+                        <div style={{}}>
                             <select name="font" id="font">
                                 <option value="Times New Roman">Times New Roman</option>
                                 <option value="Arial">Arial</option>
@@ -177,32 +139,32 @@ export default function About({isOpen, onClose}: AboutProps) {
                                 <option value="20">20</option>
                             </select>
                         </div> 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
+                        <div>
                             <p>B</p>    
                             <p>I</p>
                             <p>U</p>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button> 
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button> 
                         </div>
                         <button>X</button> 
                     </div>
                 </div>
                 <div style={{backgroundColor: '#e3dac9'}}> 
-                    <div style={mocknav}>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '20%'}}>
+                    <div id='mocknav'>
+                        <div style={{}}>
                             <select name="font" id="font">
                                 <option value="Times New Roman">Times New Roman</option>
                                 <option value="Arial">Arial</option>
@@ -218,25 +180,25 @@ export default function About({isOpen, onClose}: AboutProps) {
                                 <option value="20">20</option>
                             </select>
                         </div> 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
+                        <div>
                             <p>B</p>    
                             <p>I</p>
                             <p>U</p>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
                         </div>
 
-                        <div style={{display: 'flex', justifyContent: 'space-around', width: '10%'}}>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button>
-                            <button><img style={img} src="./src/assets/email-icon.drawio.png"/></button> 
+                        <div>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button>
+                            <button><img id='img' src="./src/assets/email-icon.drawio.png"/></button> 
                         </div>
                     </div>
                 </div>
--- portfolio/src/components/StartMenu.tsx
+++ portfolio/src/components/StartMenu.tsx
@@ -1,111 +1,127 @@
+import '../css/StartMenu.css';
+
 export default function StartMenu() {
     return (
         <>
-            <div style={{backgroundColor: 'blue', padding: '0.5vh'}}>
-                <div style={{display: 'flex', margin: '1em', alignItems: 'center'}}>
-                    <img style={{height: '5em', border: '2px solid white', borderRadius: '5%'}} src="./src/assets/neko.JPEG"/>
-                    <h1 style={{color: 'white', marginLeft: '1em'}}>Alexia</h1>
+            <div id='top'>
+                <div>
+                    <img src="./src/assets/neko.JPEG"/>
+                    <h1>Alexia</h1>
                 </div>
-            </div> 
-            
-            <main style={{display: 'flex', justifyContent: 'space-between', fontSize: '2em', height: '95vh'}}> 
-                <div className="white" style={{backgroundColor: 'white', width: '50%'}}>
+                
+            </div>  
+            <div id='orangeborder'></div>
+            <main id='main'> 
+                <div id='white'>
                     <div>
-                        <img/>
+                        <img src="./src/assets/python-removebg-preview.png"/>
                         <p>Python</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/java-removebg-preview.png"/>
                         <p>Java</p>
                     </div>
+
+                    <div id='blackborder' style={{height: '2px'}}></div>
+
                     <div>
-                        <img/>
+                        <img src="./src/assets/briefcase-removebg-preview.png"/>
                         <p>Internships</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/internet-explorer-removebg-preview.png"/>
                         <p>Web Design</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/floppy-disk-desktop-removebg-preview.png"/>
                         <p>Machine Learning</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/games-removebg-preview.png"/>
                         <p>Games</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/word-removebg-preview.png"/>
                         <p>Microsoft Word</p>
                     </div>
+
+                    <div id='blackborder' style={{height: '2px'}}></div>
+
                     <div> 
                         <p>All Programs</p>
-                        <img/>
+                        <img src="./src/assets/all-programs-removebg-preview.png"/>
                     </div>
                 </div>
 
-                <div className="blue" style={{backgroundColor: 'lightblue', width: '50%', padding: '1em'}}>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img style={{height: '2em'}} src="./src/assets/my-documents-removebg-preview.png"/>
+                <div id="blue">
+                    <div>
+                        <img src="./src/assets/my-documents-removebg-preview.png"/>
                         <p>My Documents</p>
                     </div> 
                     <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                         <div style={{display: 'flex', alignItems: 'center'}}>
-                            <img style={{height: '2em'}} src="./src/assets/my-recent-documents-removebg-preview.png"/>
+                            <img style={{height: '1.5em'}} src="./src/assets/my-recent-documents-removebg-preview.png"/>
                             <p>My Recent Documents</p>
                         </div> 
                         <p style={{fontSize: '15px'}}>&#9654;</p>
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img style={{height: '2em'}} src="./src/assets/my-pictures-removebg-preview.png"/>
+                    <div>
+                        <img src="./src/assets/my-pictures-removebg-preview.png"/>
                         <p>My Pictures</p>
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img style={{height: '2em'}} src="./src/assets/my-music-removebg-preview.png"/>
+                    <div>
+                        <img src="./src/assets/my-music-removebg-preview.png"/>
                         <p>My Music</p>
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img style={{height: '2em'}} src="https://i.imgur.com/WLQXHbd.png"/>
+                    <div>
+                        <img src="https://i.imgur.com/WLQXHbd.png"/>
                         <p>My Computer</p>
                     </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                        <img style={{height: '2em'}} src="./src/assets/control-panel-removebg-preview.png"/>
+
+                    <div id='blackborder' style={{height: '2px'}}></div>
+
+                    <div>
+                        <img src="./src/assets/control-panel-removebg-preview.png"/>
                         <p>Control Panel</p>
                     </div>
                     <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
                         <div style={{display: 'flex', alignItems: 'center'}}>
-                            <img style={{height: '2em'}} src="./src/assets/connect-to-removebg-preview.png"/>                            <p>Connect To</p>
+                            <img style={{height: '1.5em'}} src="./src/assets/connect-to-removebg-preview.png"/>                            
+                            <p>Connect To</p>
                         </div>
                         <p style={{fontSize: '15px'}}>&#9654;</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/printers-and-faxes-removebg-preview.png"/>
                         <p>Printers and Faxes</p>
                     </div>
+
+                    <div id='blackborder' style={{height: '2px'}}></div>
+
                     <div>
-                        <img/>
+                        <img src="./src/assets/download.png"/>
                         <p>Help and Support</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/search-removebg-preview.png"/>
                         <p>Search</p>
                     </div>
                     <div>
-                        <img/>
+                        <img src="./src/assets/run-removebg-preview.png"/>
                         <p>Run...</p>
                     </div>
                 </div>
             </main>
 
-            <div style={{backgroundColor: 'blue', padding: '0.5vh'}}>
-                <div style={{display: 'flex'}}>
+            <div id='bottom'>
+                <div>
                     <img style={{height: '2.5em'}} src="./src/assets/log-off-removebg-preview.png"/>
-                    <p style={{color: 'white', marginLeft: '1em'}}>Log Off</p>
+                    <h3>Log Off</h3>
                 </div>
 
-                <div style={{display: 'flex'}}>
+                <div>
                     <img style={{height: '5em'}} src="https://64.media.tumblr.com/4e932aabdfe572f340dbe32cfc06f1cb/840b4b6888f1f93f-e3/s540x810/c3d700cbea3465ba7c8215250e988acb638676e8.png"/>
-                    <p style={{color: 'white', marginLeft: '1em'}}>Turn Off computer</p>
+                    <h3>Turn Off computer</h3>
                 </div>
             </div> 
         </>
